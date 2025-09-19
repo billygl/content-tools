d9855adc81d2368db81886abaa38644af3de0f71
billygl <billy.grados@gmail.com>
2025-09-19 10:55:43 -0500

    feat: add remove silence option

--- batch.ps1
+++ batch.ps1
@@ -1,5 +1,5 @@
 # Define the base folder
-$FOLDER = "D:\_workspaces\Content\"
+$FOLDER_ROOT = "D:\_workspaces\Content\"
 
 function Create-RawFoldersFromBatch {
     param (
@@ -7,28 +7,38 @@ function Create-RawFoldersFromBatch {
     )
     $BatchFilePath = "batch.txt"
     if (Test-Path $BatchFilePath) {
-        $FolderNames = Get-Content -Path $BatchFilePath -Encoding UTF8 | Where-Object { $_.Trim() -ne "" }
-        foreach ($FolderName in $FolderNames) {
+        $FolderLines = Get-Content -Path $BatchFilePath -Encoding UTF8 | Where-Object { $_.Trim() -ne "" }
+        $Folders = @()
+        foreach ($FolderLine in $FolderLines) {
+            $Folder = ($FolderLine.Trim() -split '\|')
+            $FolderName = $Folder[0]
             $MainFolder = Join-Path -Path $BaseFolder -ChildPath $FolderName
             $RawFolder = Join-Path -Path $MainFolder -ChildPath "raw"
             New-Item -ItemType Directory -Path $RawFolder -Force | Out-Null
+            $Folders += ,$Folder
         }
-        return $FolderNames
+        return ,$Folders
     } else {
         Write-Host "No se encontró el archivo batch.txt en $BaseFolder"
     }
 }
 
-$FolderNames = Create-RawFoldersFromBatch -BaseFolder $FOLDER
+$Folders = Create-RawFoldersFromBatch -BaseFolder $FOLDER_ROOT
 #MOVE assets to raw folder
 
 Write-Host "Please, copy assets to raw folders. Press any key when it's done..."
 [void][System.Console]::ReadKey($true)
 
-foreach ($FolderName in $FolderNames) {
-    $raw_folder = Join-Path -Path $FOLDER -ChildPath "$FolderName\raw"
+foreach ($Folder in $Folders) {
+    $FolderName = $Folder[0]
+    $FolderParams = $Folder[1]
+    $raw_folder = Join-Path -Path $FOLDER_ROOT -ChildPath "$FolderName\raw"
     Write-Host "Processing folder: $raw_folder"
-    & "$PSScriptRoot\remove_silence.ps1" -Folder $raw_folder
+
+    if ($FolderParams -notmatch "-removesilence") {
+        Write-Host "Removing silence from audio files in $raw_folder"
+        & "$PSScriptRoot\remove_silence.ps1" -Folder $raw_folder
+    }
 
     $inputFiles = Get-ChildItem -Path $raw_folder -Filter "input*_out*.mp*"
     foreach ($file in $inputFiles) {
@@ -38,4 +48,5 @@ foreach ($FolderName in $FolderNames) {
 
         python "$PSScriptRoot\get_srt.py" "$raw_folder"
     }
+    
 }
